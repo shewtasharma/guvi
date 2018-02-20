18bf3422d626479b05ce412c77b3d69c53a23dc7
shewtasharma <35668200+shewtasharma@users.noreply.github.com>
2018-02-20 19:22:11 +0200

    Add files via upload

---
+++ even odd.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("no is even");
+	}
+	else
+	  printf("no is odd");
+	  return 0;
+}
