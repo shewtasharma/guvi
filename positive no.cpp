8228e769d57627e7f2e77c3a2fbf4c419dbdefd7
shewtasharma <35668200+shewtasharma@users.noreply.github.com>
2018-02-20 19:04:42 +0200

    Add files via upload

---
+++ positive no.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	  
+	  	if(n=0)
+	  	  printf("no is zero");
+	  	else if(n<0)
+	  	  printf("no is negative");
+	  	else
+	  	  printf("no is positive");
+	  }
+	  
+	return 0;
+}           
